e540be82b1d68ca564387a9ea3428766cd6044a2
krishan3333 <work.krishansingh@gmail.com>
2024-10-04 20:44:21 +0530

    first commit

---
+++ quiz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+     string question[]={"1.what is  your  name ?","2.What is the  shape  of  Earth ?","3.Who invented c language?"};
+
+
+    return 0;
+}
