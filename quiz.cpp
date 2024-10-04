8dc0ce4bae61b82299e8598df1cbe0354a8c1e81
krishan3333 <work.krishansingh@gmail.com>
2024-10-04 20:53:42 +0530

    second  commit  to  quiz

--- quiz.cpp
+++ quiz.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
      string question[]={"1.what is  your  name ?","2.What is the  shape  of  Earth ?","3.Who invented c language?"};
-
+     string anskey[][4]={""};
 
     return 0;
 }
